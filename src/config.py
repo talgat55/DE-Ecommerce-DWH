807ac554abc90c25271c27e6b772614a5570609d
lightx_desigh <lightx_design@yahoo.com>
2026-07-23 21:49:20 +0600

    fill config

---
+++ src/config.py
@@ -0,0 +1,16 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+RAW_DATA_DIR = BASE_DIR / "data" / "raw"
+
+DB_CONFIG = {
+    "config": os.getenv("DB_HOST"),
+    "port": os.getenv("DB_PORT"),
+    "dbname": os.getenv("DB_NAME"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+}
